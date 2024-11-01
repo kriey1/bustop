49b96697b2b7236836220a855a5de327f89c24d4
송동준 <dongjun569@naver.com>
2024-11-01 12:35:35 +0900

    .

--- BusApp/src/main/frontend/App.js
+++ BusApp/src/main/frontend/App.js
@@ -1,33 +1,33 @@
-// // App.js
-// import React from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import StackNavigator from './src/main/frontend/app/index'; // index.js 파일을 import
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import StackNavigator from './src/main/frontend/app/index'; // index.js 파일을 import
 
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <StackNavigator />
-//     </NavigationContainer>
-//   );
-// }
-
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
-
-function App() {
-   const [hello, setHello] = useState('')
-
-    useEffect(() => {
-        axios.get('/api/hello')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
-    }, []);
-
-    return (
-        <div>
-            백엔드에서 가져온 데이터입니다 : {hello}
-        </div>
-    );
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
 
-export default App;
+// import React, {useEffect, useState} from 'react';
+// import axios from 'axios';
+
+// function App() {
+//    const [hello, setHello] = useState('')
+
+//     useEffect(() => {
+//         axios.get('/api/hello')
+//         .then(response => setHello(response.data))
+//         .catch(error => console.log(error))
+//     }, []);
+
+//     return (
+//         <div>
+//             백엔드에서 가져온 데이터입니다 : {hello}
+//         </div>
+//     );
+// }
+// 
+// export default App;
