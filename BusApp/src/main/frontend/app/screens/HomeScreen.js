efb911ba206c897857bb518cd57ac4ec4f522f4f
sup12135 <dkstnqls1355@naver.com>
2024-11-14 13:41:26 +0900

    .

--- BusApp/src/main/frontend/app/screens/HomeScreen.js
+++ BusApp/src/main/frontend/app/screens/HomeScreen.js
@@ -12,6 +12,20 @@ function HomeScreen({ navigation }) {
       <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('Login')}>
         <Text style={styles.loginButtonText}>로그인</Text>
       </TouchableOpacity>
+=======
+    <View>
+      <Text>Home Screen</Text>
+      <Text>운전자 혹은 보호자이신가요?</Text>
+      
+      <Button
+        title="회원가입" // 운전자 또는 보호자 선택페이지
+        onPress={() => navigation.navigate('SignupScreen')}
+      />
+      <Button
+        title="로그인"
+        onPress={() => navigation.navigate('Login')}
+      />
+>>>>>>> Stashed changes
 
       {/* 회원가입 버튼 */}
       <TouchableOpacity onPress={() => navigation.navigate('SignupScreen')}>
